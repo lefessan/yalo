1e5c748f60d5339dab02d28744d6b5261ec66b95
lefessan <fabrice.le_fessant@ocamlpro.com>
2025-11-01 13:38:15 +0000

    Deploying to gh-pages from  @ 00040b5e2120c0a120940329ef330cd9a359c001 🚀

---
+++ lints/groups.js
@@ -0,0 +1,19 @@
+
+var NAMESPACES_FILTERS_DEFAULT = {
+    "FIND": true,
+    "YALO": true };
+var IMPL_FILTERS_DEFAULT = {
+    "yalo_plugin_FIND": true,
+    "yalo_plugin_YALO": true };
+var LEVEL_FILTERS_DEFAULT = {
+    allow: true,
+    warn: true,
+    deny: true
+};
+var GROUPS_FILTER_DEFAULT = {
+    "autofix": true,
+    "find": true,
+    "line": true,
+    "typed": true,
+    "untyped": true
+};
---
+++ sphinx/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: 'master (00040b5) (2025/11/01 13:38)',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
